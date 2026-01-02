f154819ba855891db3e9f54ace9342cd4f7bc06f
Vassia Christ <christvassia@gmail.com>
2026-01-02 14:58:54 +0100

    glass look

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/no-unresolved
 import { REACT_NATIVE_SERVER_URL } from "@env";
+import { BlurView } from 'expo-blur';
 import * as FileSystem from "expo-file-system/legacy";
 import * as ImageManipulator from "expo-image-manipulator";
 import * as ImagePicker from "expo-image-picker";
@@ -18,6 +19,7 @@ import {
 } from "react-native";
 import { Colors } from "../../components/colors";
 import { useDocuments } from '../context/DocumentsContext';
+
 
 const SERVER_URL = `${REACT_NATIVE_SERVER_URL}/ocr`;
 
@@ -164,9 +166,18 @@ export default function OCRScreen() {
       {!loading && (
         <>
           {!docxUrl && (
-            <TouchableOpacity style={styles.pickFile} onPress={pickImage}>
-              <Text style={styles.ButtonText}>Pick Image</Text>
-            </TouchableOpacity>
+            <>
+              <BlurView intensity={100} tint="light" style={styles.glassContainer}>
+                <Text style={styles.text}>
+                  Take a photo or choose an image from your gallery to convert it in a docx file.
+                </Text>
+              </BlurView>
+
+              <TouchableOpacity style={styles.pickFile} onPress={pickImage}>
+                <Text style={styles.ButtonText}>Pick Image</Text>
+              </TouchableOpacity>
+            </>
+
           )}
           {docxUrl && (
             <>
@@ -198,7 +209,7 @@ const styles = StyleSheet.create({
     height: 360,
     position: "relative",
     overflow: "hidden",
-    marginBottom: 100
+    marginBottom: 50
   },
   headerImage: {
     // aspectRatio: 16 / 9,
@@ -217,6 +228,25 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+  glassContainer: {
+    margin: 30,
+    borderRadius: 20,
+    backgroundColor: "rgba(49, 44, 81, 0.25)",
+    borderWidth: 1,
+    borderColor: 'rgba(255, 255, 255, 0.3)', // subtle glass border
+    overflow: 'hidden',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    padding: 30,
+    color: Colors.backgroundTabs,
+    justifyContent: "center",
+    alignItems: "center",
+    lineHeight: 25,
+    fontSize: 18,
+    elevation: 6,
+  },
   pickFile: {
     backgroundColor: Colors.primary,
     paddingVertical: 16,
@@ -224,7 +254,6 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     alignItems: "center",
     justifyContent: "center",
-    marginVertical: 10,
     shadowColor: "#000",
     shadowOffset: { width: 0, height: 4 },
     shadowOpacity: 0.25,
@@ -248,11 +277,11 @@ const styles = StyleSheet.create({
   DownloadButtonText: {
     color: Colors.primary,
     fontWeight: "600",
-    fontSize: 16,
+    fontSize: 18,
   },
   ButtonText: {
     color: Colors.background,
     fontWeight: "600",
-    fontSize: 16,
+    fontSize: 18,
   },
 });
